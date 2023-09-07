bd7946198d29a692c6ce923f8c0d1c23cf13485f
Ayodele Ayodeji <awesomedursky@gmail.com>
2023-09-07 17:06:30 +0100

    task completed

---
+++ app.js
@@ -0,0 +1,13 @@
+// geting current weekday of the week
+const weekList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const day = new Date();
+let getDay = weekList[day.getDay()];
+const currentWeekDay = document.querySelector('#currentDayOfTheWeek');
+currentWeekDay.innerHTML+=getDay
+
+
+//getting current UTC Time
+
+const utc = new Date().toUTCString();
+const currentUTCTime = document.querySelector('#currentUTCTime');
+currentUTCTime.innerHTML+=utc
